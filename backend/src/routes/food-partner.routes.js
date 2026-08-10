cc3cc061baf32afbeb7ba418c1d7d65b8cf1c918
Akash Dubey <yogesh8853599651@gmail.com>
2026-08-10 14:52:08 +0530

    feat: update food partner route to use authAnyMiddleware for getFoodPartnerById

--- backend/src/routes/food-partner.routes.js
+++ backend/src/routes/food-partner.routes.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 const { getFoodPartnerById,changePassword,editFoodPartnerProfile } = require("../controllers/food-partner.controller");
-const {authFoodPartnerMiddleware,authUserMiddleware} = require("../middlewares/auth.middleware")
+const { authFoodPartnerMiddleware, authAnyMiddleware } = require("../middlewares/auth.middleware");
 
-router.get("/:id",
-authUserMiddleware,
-    getFoodPartnerById);
+router.get("/:id", authAnyMiddleware, getFoodPartnerById);
 router.patch("/edit-profile",authFoodPartnerMiddleware,editFoodPartnerProfile);
 router.patch("/change-password",authFoodPartnerMiddleware,changePassword);
 
